7a0cd336ffcfd753dd047266f3cb0eeac6008085
zizon233 <zizon233@nate.com>
2019-11-09 12:57:06 +0900

    Bubble Sort

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 using namespace std;
 
-void bubble_sort(vector<int> arr) {
+void bubble_sort(vector<int> &arr) {
 	for (int i = 0; i < arr.size(); i++) {
 		for (int j = i + 1; j < arr.size(); j++) {
 			if (arr[i] > arr[j]) swap(arr[i], arr[j]);
